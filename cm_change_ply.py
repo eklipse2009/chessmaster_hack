10f8503f0a67ace0af45189b2d75ed521c1554b6
Lady Eklipse <eklipse2009@gmail.com>
2020-04-19 11:19:07 +0300

    Made user-friendly dialogs and an executable.

--- cm_change_ply.py
+++ cm_change_ply.py
@@ -4,27 +4,32 @@ import sys
 import binascii
 import argparse
 
-# folder = "D:\\games\\Chessmaster Grandmaster Edition\\Data\\Personalities"
-# os.chdir(folder)
-
 PERSONALITIES_FOLDER = "Data\\Personalities"
-
+# if not hasattr(sys, 'frozen'):
+#     os.chdir("D:\\Games\\Chessmaster Grand Master Edition 1.20 RUS")
 
 def backup():
     global PERSONALITIES_FOLDER
     backup_folder = os.path.join(PERSONALITIES_FOLDER, 'backup')
+    print(PERSONALITIES_FOLDER)
     os.makedirs(backup_folder, exist_ok=True)
     for file in os.listdir(PERSONALITIES_FOLDER):
-        if not file.endswith('.CMP'):
+        # print(file)
+        if not file.lower().endswith('.cmp'):
             continue
         backup_file = os.path.join(backup_folder, os.path.basename(file))
         # print(file, backup_file)
         if not os.path.exists(backup_file):
             shutil.copy(file, backup_file)
+            # print("backed up ", file)
+        # else:
+        #     print(backup_file, "exists")
 
 def replace_ply(personality_file, desirable_ply):
-    file_path = os.path.join('backup', personality_file)
+    global PERSONALITIES_FOLDER
+    file_path = os.path.join(PERSONALITIES_FOLDER, 'backup', personality_file)
     if not os.path.exists(file_path):
+        # print(file_path, 'does not exist')
         return 0
     with open(file_path, 'rb') as f:
         contents = f.read()
@@ -46,7 +51,7 @@ def replace_ply(personality_file, desirable_ply):
         parts[part_num] = hex(desirable_ply)[2:].zfill(2).encode() + parts[part_num][2:]
         hex_contents = b'64000000'.join(parts)
         contents = binascii.unhexlify(hex_contents)
-        with open(personality_file, 'wb') as fw:
+        with open(os.path.join(PERSONALITIES_FOLDER, personality_file), 'wb') as fw:
             fw.write(contents)
         return 1
 
@@ -54,7 +59,7 @@ def replace_ply(personality_file, desirable_ply):
 def replace_ply_for_all_personalities(folder, ply):
     modded_files = 0
     for file in os.listdir(folder):
-        if not file.endswith('.CMP'):
+        if not file.lower().endswith('.cmp'):
             continue
         modded_files += replace_ply(file,ply)
     print("Modded "+str(modded_files)+" personality files.")
@@ -62,13 +67,13 @@ def replace_ply_for_all_personalities(folder, ply):
 def main():
     global PERSONALITIES_FOLDER
     parser = argparse.ArgumentParser(description="Change ply count for all personalities in Chessmaster X and Grandmaster")
-    parser.add_argument('--cmpath', help="Folder where Chessmaster is installed.")
-    parser.add_argument('--ply', help="Max desirable ply for all personalities.", type=int, default=10)
+    parser.add_argument('--cmpath', help="Folder where Chessmaster is installed.", default="")
+    parser.add_argument('--ply', help="Max desirable ply for all personalities.", type=int)
     args = parser.parse_args()
     if not os.path.exists(PERSONALITIES_FOLDER) and not args.cmpath:
         print("Chessmaster personalities not found. Place this script into the folder where Chessmaster is installed or set the --cmpath parameter.")
         end()
-    elif not os.path.exists(args.cmpath):
+    elif args.cmpath and not os.path.exists(args.cmpath):
         print("Path "+args.cmpath+" does not exist.")
         end()
     else:
@@ -76,7 +81,8 @@ def main():
         if not os.path.exists(PERSONALITIES_FOLDER):
             print("Folder "+PERSONALITIES_FOLDER+" does not exist. Cannot fine personalities to modify.")
             end()
-    os.chdir(PERSONALITIES_FOLDER)
+    if not args.ply:
+        args.ply = int(input("Enter max desirable ply for all characters (10 is recommended)\n"))
     print("Making backup of original personalities...")
     backup()
     print("Will modify all personalities to use ply="+str(args.ply))
@@ -86,7 +92,6 @@ def main():
 
 def end():
     os.system("pause")
-    # k=input("Press any key to exit...")
     sys.exit()
 
 if __name__=='__main__':
